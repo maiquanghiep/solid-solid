21808f7e7a7077c7c1f86e62a325f6508e1ee2b7
Mai Quang Hiep <maiquanghiepdl@gmail.com>
2024-02-05 14:29:10 +0700

    Add graceful shutdown

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -7,9 +7,14 @@ import { RootModule } from './root/root.module';
 import { HealthModule } from './health/health.module';
 import { MetricsModule } from './metrics/metrics.module';
 import { ConfigModule } from '@nestjs/config';
+import { GracefulShutdownModule } from 'nestjs-graceful-shutdown';
 
 @Module({
   imports: [
+    /* Found a interesting article about graceful shutdown 
+    https://dev.to/hienngm/graceful-shutdown-in-nestjs-ensuring-smooth-application-termination-4e5n
+    */
+    GracefulShutdownModule.forRoot(),
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING),
     MongooseModule.forFeature([
